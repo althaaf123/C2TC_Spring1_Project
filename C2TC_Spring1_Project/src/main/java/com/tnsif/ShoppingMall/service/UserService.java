6a1247d986bdd904c41ded87cc41c9a697c5fb97
althaaf123 <syedalthaaf4@gmail.com>
2025-10-17 18:46:16 +0530

    project

---
+++ C2TC_Spring1_Project/src/main/java/com/tnsif/ShoppingMall/service/UserService.java
@@ -0,0 +1,32 @@
+package com.mall.service;
+
+import com.mall.entity.User;
+import com.mall.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
